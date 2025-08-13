ab9f18fe9ca1eea8a0a268619add3264d866d314
Shubham Bera <121177037+Shubham-2004@users.noreply.github.com>
2025-08-13 15:12:13 +0530

    Update : 50 mb file size to 10 mb

--- app.js
+++ app.js
@@ -19,8 +19,8 @@ cloudinary.config({
 });
 
 app.use(cors());
-app.use(express.json({ limit: '50mb' }));
-app.use(express.urlencoded({ extended: true, limit: '50mb' }));
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 
 const uploadsDir = path.join(__dirname, 'uploads');
 if (!fs.existsSync(uploadsDir)) {
